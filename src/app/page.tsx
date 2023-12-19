c44bf5e161fe9c197a66c63c85ffc5aa9b76767c
illuminarean-mg <myunggyu.lee@illuminarean.com>
2023-12-19 12:25:28 +0900

    image change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <Image src="/sample.png" width="600" height="400" alt="sample image" />
+    <div
+      style={{
+        height: 400,
+        position: "relative",
+      }}
+    >
+      <Image src="/sample.png" alt="sample image" fill />
+    </div>
   );
 }
